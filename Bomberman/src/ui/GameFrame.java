7124e70c3730322e31f9cf89b4b19929de90602f
Zydrick Jan Delima <zmdelima@up.edu.ph>
2016-11-10 14:55:50 +0800

    Update GameFrame.java
    Edited import "merge conflict"

--- Bomberman/src/ui/GameFrame.java
+++ Bomberman/src/ui/GameFrame.java
@@ -3,10 +3,10 @@ package ui;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Canvas;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Canvas;
 
 import game.Game;
 
