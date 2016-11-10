f495a565f4065804c65241830c7125786feeadfc
Michael Jay Braga <majcbraga@gmail.com>
2016-11-10 17:48:47 +0800

    Added UDP port in Constants class

--- Bomberman/src/constants/Constants.java
+++ Bomberman/src/constants/Constants.java
@@ -2,5 +2,6 @@ package constants;
 
 public class Constants {
 	public static final int TCP_PORT = 8080;
+	public static final int UDP_PORT = 9090;
 	public static final String LEAVE_GAME = "744747183594e050a76b3cc00bf614f9d1e31198";
 }
