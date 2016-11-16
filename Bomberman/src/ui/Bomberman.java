cea49bd42730d565558e0c0627c17e4d48815f7e
zmdelima <zmdelima@up.edu.ph>
2016-11-16 22:44:24 +0800

    Create collision on sprite to tileset

--- Bomberman/src/ui/Bomberman.java
+++ Bomberman/src/ui/Bomberman.java
@@ -13,7 +13,8 @@ public class Bomberman extends BasicGame{
 	private TiledMap map;
 	private Animation sprite, up, down, left, right;
 	private float x= 1f, y=1f;
-
+	private boolean[][] blocked;
+	
 	public Bomberman() {
 		super("Bomberman");
 		
@@ -53,32 +54,61 @@ public class Bomberman extends BasicGame{
 		// TODO Auto-generated method stub
 		Input input = arg0.getInput();
 		long delta = (long) 1f; 
-		if (input.isKeyDown(Input.KEY_UP))
-		{
+		int id;
+		int pX,pY;
+		System.out.println("Coord:"+x+","+y);
+		if (input.isKeyDown(Input.KEY_UP)){
 		    sprite = up;
 		    sprite.update(delta);
-		    // The lower the delta the slowest the sprite will animate.
-		    y -= delta * 0.05f;
+		    //System.out.println("@up");
+		    pY = (int)Math.ceil(y);
+		    id = map.getTileId((int)x,(int)Math.ceil(y)-1,0);
+		    if(id < 2){
+			    // The lower the delta the slowest the sprite will animate.
+			    y -= delta * 0.05f;
+		    } else {
+		    	y = pY;
+		    }
 		}
 		else if (input.isKeyDown(Input.KEY_DOWN))
 		{
 		    sprite = down;
 		    sprite.update(delta);
-		    y += delta * 0.05f;
+		    //System.out.println("@down");
+		    pY = (int)y;
+		    id = map.getTileId((int)x,(int)(y+1),0);
+		    if(id < 2){
+		    	y += delta * 0.05f;
+		    } else {
+		    	y = pY;
+		    }
 		}
 		else if (input.isKeyDown(Input.KEY_LEFT))
 		{
 		    sprite = left;
 		    sprite.update(delta);
-		    x -= delta * 0.05f;
+		    //System.out.println("@left");
+		    pX = (int)Math.ceil(x);
+		    id = map.getTileId((int)Math.ceil(x)-1,(int)y,0);
+		    if(id < 2){
+		    	x -= delta * 0.05f;
+		    } else {
+		    	x = pX;
+		    }
 		}
 		else if (input.isKeyDown(Input.KEY_RIGHT))
 		{
 		    sprite = right;
 		    sprite.update(delta);
-		    x += delta * 0.05f;
+		    //System.out.println("@right");
+		    pX = (int)x;
+		    id = map.getTileId((int)(x+1),(int)y,0);
+		    if(id < 2){
+		    	x += delta * 0.05f;
+		    } else {
+		    	x = pX;
+		    }
 		}
-
 	}
 	
 }
